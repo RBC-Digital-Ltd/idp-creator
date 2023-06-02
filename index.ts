512e6d208a56f6d319cc476ba1a00f9bf05fd1ee
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-06-02 23:05:43 +0100

    Add github workflow

--- index.ts
+++ index.ts
@@ -3,37 +3,53 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import * as awsNative from "@pulumi/aws-native";
 
-// Create an IDP Provider
-const idp = new awsNative.iam.OIDCProvider("github", {
-  thumbprintList: ["6938fd4d98bab03faadb97b34396831e3780aea1"],
-  url: "https://token.actions.githubusercontent.com",
-  clientIdList: ["sts.amazonaws.com"],
+const provider = new awsNative.Provider("provider", {
+  region: "eu-west-1",
 });
 
-const role = new awsNative.iam.Role("GithubTerraformDeployRole", {
-  managedPolicyArns: ["arn:aws:iam::aws:policy/AdministratorAccess"],
-  assumeRolePolicyDocument: JSON.stringify({
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Action: "sts:AssumeRoleWithWebIdentity",
-        Effect: "Allow",
-        Principal: {
-          Federated: idp.arn,
-        },
-        Condition: {
-          StringLike: {
-            "token.actions.githubusercontent.com:sub":
-              "repo:RBC-Digital-Ltd/*:*",
+// Create an IDP Provider
+const idp = new awsNative.iam.OIDCProvider(
+  "github",
+  {
+    thumbprintList: ["6938fd4d98bab03faadb97b34396831e3780aea1"],
+    url: "https://token.actions.githubusercontent.com",
+    clientIdList: ["sts.amazonaws.com"],
+  },
+  {
+    provider,
+  }
+);
+
+const role = new awsNative.iam.Role(
+  "GithubTerraformDeployRole",
+  {
+    managedPolicyArns: ["arn:aws:iam::aws:policy/AdministratorAccess"],
+    assumeRolePolicyDocument: JSON.stringify({
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: "sts:AssumeRoleWithWebIdentity",
+          Effect: "Allow",
+          Principal: {
+            Federated: idp.arn,
           },
-          StringEquals: {
-            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+          Condition: {
+            StringLike: {
+              "token.actions.githubusercontent.com:sub":
+                "repo:RBC-Digital-Ltd/*:*",
+            },
+            StringEquals: {
+              "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+            },
           },
         },
-      },
-    ],
-  }),
-});
+      ],
+    }),
+  },
+  {
+    provider,
+  }
+);
 
 // Export the name of the bucket
 export const roleName = role.roleName;
