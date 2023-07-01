c313e255ecf7855472618996b90e2fcbba0346db
Rahul Parkar <rahul.a.parkar@gmail.com>
2023-07-01 13:50:26 +0100

    Add role policy for IAM pieces

--- index.ts
+++ index.ts
@@ -1,19 +1,42 @@
 import * as awsNative from "@pulumi/aws-native";
 
 // Create an IDP Provider
-const idp = new awsNative.iam.OIDCProvider(
-  "github",
-  {
-    thumbprintList: ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a3a8518e8759bf075b76b750d4f2df264fcd"],
-    url: "https://token.actions.githubusercontent.com",
-    clientIdList: ["sts.amazonaws.com"],
-  },
-);
+const idp = new awsNative.iam.OIDCProvider("github", {
+  thumbprintList: [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+  ],
+  url: "https://token.actions.githubusercontent.com",
+  clientIdList: ["sts.amazonaws.com"],
+});
 
 const role = new awsNative.iam.Role(
   "GithubTerraformDeployRole",
   {
-    managedPolicyArns: ["arn:aws:iam::aws:policy/AWSCloudFormationFullAccess", "arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/AWSLambda_FullAccess", "arn:aws:iam::aws:policy/CloudWatchFullAccess", "arn:aws:iam::aws:policy/AmazonAPIGatewayAdministrator"],
+    managedPolicyArns: [
+      "arn:aws:iam::aws:policy/AWSCloudFormationFullAccess",
+      "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+      "arn:aws:iam::aws:policy/AWSLambda_FullAccess",
+      "arn:aws:iam::aws:policy/CloudWatchFullAccess",
+      "arn:aws:iam::aws:policy/AmazonAPIGatewayAdministrator",
+    ],
+    policies: [
+      {
+        policyName: "GithubTerraformDeployPolicy",
+        policyDocument: JSON.stringify({
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Effect: "Allow",
+              Action: ["iam:CreateRole", "iam:DeleteRole", "iam:PutRolePolicy"],
+              Resource: [
+                "*",
+              ],
+            },
+          ],
+        }),
+      },
+    ],
     assumeRolePolicyDocument: JSON.stringify({
       Version: "2012-10-17",
       Statement: [
@@ -26,7 +49,8 @@ const role = new awsNative.iam.Role(
           Action: "sts:AssumeRoleWithWebIdentity",
           Condition: {
             StringLike: {
-              "token.actions.githubusercontent.com:sub": "repo:RBC-Digital-Ltd/*"
+              "token.actions.githubusercontent.com:sub":
+                "repo:RBC-Digital-Ltd/*",
             },
             StringEquals: {
               "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
@@ -37,7 +61,7 @@ const role = new awsNative.iam.Role(
     }),
   },
   {
-    dependsOn: idp
+    dependsOn: idp,
   }
 );
 
